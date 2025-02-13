10980f247ffa9fd4435f486a90c813f5b2bc12c5
Yessir11 <yessirnoudo1997@gmail.com>
2025-02-13 23:34:46 +0300

    Fix Duplicated actions in python server

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    #return redirect("/profile")
-    return render_template("index.html")
+    return redirect("/profile")
 
 @app.route("/profile")
 def profile():
